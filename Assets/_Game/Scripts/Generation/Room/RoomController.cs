ac2252d0fca2fcdd32de8ce38c10b2c52266e38d
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-24 16:58:23 +0200

    Walls generation

--- Assets/_Game/Scripts/Generation/Room/RoomController.cs
+++ Assets/_Game/Scripts/Generation/Room/RoomController.cs
@@ -16,6 +16,7 @@ public class RoomController: MonoBehaviour
     public void Build(BoxItemConfig boxItem) 
     {
         SetFloor();
+        SetWalls();
         SetObjects(boxItem);
     }
 
@@ -32,6 +33,55 @@ public class RoomController: MonoBehaviour
                 SetRandomRange(_floorMap, _config.FloorTiles);
                 break;
         }
+    }
+
+    private void SetWalls()
+    {
+        switch (_config.WallTiles.Length)
+        {
+            case 0:
+                return;
+            case 1:
+                SetSimpleLine(_wallMap, _config.WallTiles[0]);
+                break;
+            default:
+                SetRandomLine(_wallMap, _config.WallTiles);
+                break;
+        }
+    }
+
+    private void SetSimpleLine(Tilemap tilemap, TileBase tile)
+    {
+        var halfSize = _config.Size / 2;
+        for (var x = -halfSize + 1; x <= halfSize; x++)
+        {
+            tilemap.SetTile(new Vector3Int(x, halfSize, 0), tile);
+        }
+        
+        for (var y = -halfSize + 1; y < halfSize; y++)
+        {
+            tilemap.SetTile(new Vector3Int(halfSize, y, 0), tile);
+        }
+        
+        tilemap.RefreshAllTiles();
+    }
+    
+    private void SetRandomLine(Tilemap tilemap, TileBase[] tiles)
+    {
+        var halfSize = _config.Size / 2;
+        for (var x = -halfSize + 1; x <= halfSize; x++)
+        {
+            var rand = Random.Range(0, tiles.Length);
+            tilemap.SetTile(new Vector3Int(x, halfSize, 0), tiles[rand]);
+        }
+        
+        for (var y = -halfSize + 1; y < halfSize; y++)
+        {
+            var rand = Random.Range(0, tiles.Length);
+            tilemap.SetTile(new Vector3Int(halfSize, y, 0), tiles[rand]);
+        }
+        
+        tilemap.RefreshAllTiles();
     }
     
     private void SetSimpleRange(Tilemap tilemap, TileBase tile)
